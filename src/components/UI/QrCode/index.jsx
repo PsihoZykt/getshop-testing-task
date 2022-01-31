bfadc25c1dccd66879bf66c2f149055088a4de2e
psihozykt <psihozykt@gmail.com>
2022-01-31 16:45:13 +0900

    initial setup. First version  of banner-entry

---
+++ src/components/UI/QrCode/index.jsx
@@ -0,0 +1,10 @@
+import qrCode from "../../../assets/img/qrCode.png";
+import React from "react";
+
+const QrCode = () => {
+    return (
+            <img src={qrCode} alt="QR code leads to getshoptv.com"/>
+    );
+};
+
+export default QrCode;
