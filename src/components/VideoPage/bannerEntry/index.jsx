97780e3a94b5a8d36cc87541c4ff8ff90234335a
psihozykt <psihozykt@gmail.com>
2022-02-03 15:56:11 +0900

    added gh pages

--- src/components/VideoPage/bannerEntry/index.jsx
+++ src/components/VideoPage/bannerEntry/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import s from './bannerEntry.module.css';
 import QrCode from '../../UI/QrCode';
 
@@ -6,24 +7,30 @@ function BannerEntry({ switchToPhonePage }) {
   return (
     <div className={s.bannerEntry}>
       <h2>
-        {' '}
         Исполните мечту вашего малыша!
         <br />
-        {' '}
         Подарите ему собаку!
-        {' '}
       </h2>
       <QrCode />
       <div>
-        {' '}
         Сканируйте QR-код
         <br />
-        {' '}
         или нажмите ОК
       </div>
-      <div onClick={switchToPhonePage} className={s.button}> OK</div>
+      <div
+        role="button"
+        onKeyPress={switchToPhonePage}
+        tabIndex={0}
+        onClick={switchToPhonePage}
+        className={s.button}
+      >
+        OK
+      </div>
     </div>
   );
 }
 
+BannerEntry.propTypes = {
+  switchToPhonePage: PropTypes.func.isRequired,
+};
 export default BannerEntry;
